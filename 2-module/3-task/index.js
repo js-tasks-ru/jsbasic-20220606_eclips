8ec87a91f3ddb2d182f1d20d66e5aaaefbe5fc89
GMBV <insideTheUnreal@gmail.com>
2022-06-14 14:08:23 +0700

    1&2 module. First commit, all tests clear

--- 2-module/3-task/index.js
+++ 2-module/3-task/index.js
@@ -1,5 +1,16 @@
 let calculator = {
-  // ваш код
+  memory : [],
+  read(a,b) {
+    this.memory = [];
+    this.memory.push(a);
+    this.memory.push(b);
+  },
+  sum() {
+    return this.memory.reduce((a, b) => a + b);
+  },
+  mul() {
+    return this.memory.reduce((a, b) => a * b);
+  },
 };
 
 // НЕ УДАЛЯТЬ СТРОКУ, НУЖНА ДЛЯ ПРОВЕРКИ
