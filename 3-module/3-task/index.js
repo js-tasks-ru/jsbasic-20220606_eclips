d4295f6bb2869f8163f16c72fbca3f4e68cbc69f
GMBV <insideTheUnreal@gmail.com>
2022-06-15 22:21:54 +0700

    Third module, all test completed, but there is referenceError

--- 3-module/3-task/index.js
+++ 3-module/3-task/index.js
@@ -1,3 +1,9 @@
+let fs = require("fs")
+let vm = require('vm')
+vm.runInThisContext(fs.readFileSync(__dirname + "/../../1-module/3-task/index.js"))
+
 function camelize(str) {
-  // ваш код...
+  let arr = str.split("-");
+  let camelized = arr.shift() + arr.map(ucFirst).reduce( (a,b)=> a + b, "");
+  return camelized;
 }
