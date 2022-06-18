6967e4e0396a3be1d2190bbc0fa76e166c003f9f
GMBV <insideTheUnreal@gmail.com>
2022-06-18 17:15:56 +0700

    1&2&3 task, all test completed

--- 4-module/1-task/index.js
+++ 4-module/1-task/index.js
@@ -1,3 +1,9 @@
 function makeFriendsList(friends) {
-  // ваш код...
+  let newElement = document.createElement('ul');
+  let list = "";
+  friends
+          .map( (f)=> f.firstName + " " + f.lastName )
+          .forEach( (curr)=> list += `<li>${curr}</li>` );
+  newElement.innerHTML = "<ul>" + list + "</ul>";
+  return newElement;
 }
